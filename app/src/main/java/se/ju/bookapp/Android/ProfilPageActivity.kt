5feed0921f5c881e44f6a5103372276d30123170
aljonasamuelsson <aljonasamuelsson@hotmail.com>
2022-02-16 15:21:30 +0100

    Added xml-files and Activities

---
+++ app/src/main/java/se/ju/bookapp/Android/ProfilPageActivity.kt
@@ -0,0 +1,11 @@
+package se.ju.bookapp.Android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_page)
+    }
+}
