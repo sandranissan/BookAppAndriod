ba1fe4ad2c4188a26f3c260b13c4bbc4ba75fcb1
sandranissan <sandranisssan@outlook.com>
2022-02-22 15:47:10 +0100

    Created navbar(working)

--- app/src/main/java/se/ju/bookapp/Android/MainActivity.kt
+++ app/src/main/java/se/ju/bookapp/Android/MainActivity.kt
@@ -1,11 +1,48 @@
 package se.ju.bookapp.Android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import se.ju.bookapp.Android.fragments.DiscoverFragment
+import se.ju.bookapp.Android.fragments.MyBooksFragment
+import se.ju.bookapp.Android.fragments.ProfileFragment
+import se.ju.bookapp.Android.fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val discoverFragment = DiscoverFragment()
+        //val myBooksFragment = MyBooksFragment()
+        //val profileFragment = ProfileFragment()
+        //val searchFragment = SearchFragment()
+
+        //makeCurrentFragment(discoverFragment)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavigation.setupWithNavController(navController)
+
+
+
+
+
     }
+
+   // private fun makeCurrentFragment(fragment: Fragment) =
+        //    supportFragmentManager.beginTransaction().apply {
+         //       replace(R.id.fragment_container, fragment)
+        //        commit()
+        //}
 }
