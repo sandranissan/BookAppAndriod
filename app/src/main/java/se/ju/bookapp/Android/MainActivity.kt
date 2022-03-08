9ca271bbf6a0e953a8e5d19512c7155e70ef82fe
aljonasamuelsson <aljonasamuelsson@hotmail.com>
2022-03-08 16:31:58 +0100

    fixade så att man kan logga in med fb och att det läggs in i firebase

--- app/src/main/java/se/ju/bookapp/Android/MainActivity.kt
+++ app/src/main/java/se/ju/bookapp/Android/MainActivity.kt
@@ -13,14 +13,37 @@ import se.ju.bookapp.Android.fragments.DiscoverFragment
 import se.ju.bookapp.Android.fragments.MyBooksFragment
 import se.ju.bookapp.Android.fragments.ProfileFragment
 import se.ju.bookapp.Android.fragments.SearchFragment
+import android.widget.Toast
+import com.facebook.AccessToken
+import com.google.firebase.ktx.Firebase
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_sign_in_page.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    var firebaseAuth: FirebaseAuth?=null
+    var callbackManager: CallbackManager?=null
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAuth = FirebaseAuth.getInstance()
+        callbackManager = CallbackManager.Factory.create()
+
+        login_button.setReadPermissions("email")
+        login_button.setOnClickListener {
+            signIn()
+        }
+
+
 
         //val discoverFragment = DiscoverFragment()
         //val myBooksFragment = MyBooksFragment()
@@ -40,7 +63,47 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-   // private fun makeCurrentFragment(fragment: Fragment) =
+    private fun signIn() {
+        login_button.registerCallback(callbackManager, object:FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult) {
+                handleFacebookAccessToken(result!!.accessToken)
+            }
+
+            override fun onCancel() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(error: FacebookException) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+
+    private fun handleFacebookAccessToken(accessToken: AccessToken?) {
+        // get credential
+        val credential = FacebookAuthProvider.getCredential(accessToken!!.token)
+        firebaseAuth!!.signInWithCredential(credential)
+            .addOnFailureListener { e->
+                Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+            }
+            .addOnSuccessListener { result ->
+
+                //get email
+                val email =  result.user!!.email
+                Toast.makeText(this, "du loggade in med detta email:" + email, Toast.LENGTH_LONG).show()
+            }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager!!.onActivityResult(requestCode,resultCode, data)
+
+    }
+
+
+    // private fun makeCurrentFragment(fragment: Fragment) =
         //    supportFragmentManager.beginTransaction().apply {
          //       replace(R.id.fragment_container, fragment)
         //        commit()
